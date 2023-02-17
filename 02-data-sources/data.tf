358a5367bf5efade8bd9afe14f5066a8bb8a472b
LokeshV <lokeshviswa428@gmail.com>
2023-02-17 23:00:49 +0530

    Roboshop Implementation in Terraform

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -10,19 +10,19 @@ output "ami" {
 
 
 
-#
-#output "public_ip" {
-#  value = data.aws_instance.foo.public_ip
-#}
-#
-#output "private_ip" {
-#  value = data.aws_instance.foo.private_ip
-#}
-#
-#data "aws_instance" "foo" {
-#  instance_id = "i-0d5fc7fb99c768588"
-#}
-#
-#provider "aws" {
-#  region = "us-east-1"
-#}
+
+output "public_ip" {
+  value = data.aws_instance.foo.public_ip
+}
+
+output "private_ip" {
+  value = data.aws_instance.foo.private_ip
+}
+
+data "aws_instance" "foo" {
+  instance_id = "i-01f3f39bbedf195e0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
